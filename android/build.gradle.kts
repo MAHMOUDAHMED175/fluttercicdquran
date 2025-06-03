f1341bec037a1b298ec2dd2263172a4d6aef6d10
MAHMOUDAHMED175 <eng.mahmoudahmed175@gmail.com>
2025-06-03 18:24:53 +0300

    new branch

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,16 @@
-allprojects {
+buildscript {
+    val kotlin_version: String by extra("1.9.24")
     repositories {
         google()
         mavenCentral()
     }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+//        classpath("com.android.tools.build:gradle:8.3.0") // أو إصدارك الحالي
+    }
 }
+
+
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
@@ -23,6 +30,8 @@ plugins {
     // ...
 
     // Add the dependency for the Google services Gradle plugin
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+
 
 }
